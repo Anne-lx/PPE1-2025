72ba65ac8b41343a0c9bb6cda133a06505189bf1
Anne-lx <maria.chamb@laposte.net>
2025-10-20 20:11:30 +0200

    Ajout boucle for dans comptes.sh

--- Exercices/comptes.sh
+++ Exercices/comptes.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/bash
+for ELEMENT in 2016 2017 2018
+do
+    echo "Nombre de lieux $ELEMENT"
+    cat /media/anne/Disque/Archive/Exercice1/ann/$ELEMENT/*.ann | grep Location | wc -l
+done
 
-echo "Nombre de lieux en 2016:"
-cat /media/anne/Disque/Archive/Exercice1/ann/2016/2016*.ann | grep Location | wc -l
-echo "Nombre de lieux en 2017:"
-cat /media/anne/Disque/Archive/Exercice1/ann/2017/* | grep Location | wc -l
-echo "Nombre de lieux en 2018:"
-cat /media/anne/Disque/Archive/Exercice1/ann/2018/* | grep Location | wc -l
+#echo "Nombre de lieux en 2016:"
+#cat /media/anne/Disque/Archive/Exercice1/ann/#2016/2016*.ann | grep Location | wc -l
+#echo "Nombre de lieux en 2017:"
+#cat /media/anne/Disque/Archive/Exercice1/ann/2017/* | grep Location | wc -l
+#echo "Nombre de lieux en 2018:"
+#cat /media/anne/Disque/Archive/Exercice1/ann/2018/* | #grep Location | wc -l
 
 
 
